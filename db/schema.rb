9bbd94b349916e49bdba4e8fb426b77ebaf903d9
Akiko <chigumaya925@gmail.com>
2017-09-29 09:39:43 +0000

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170903105051) do
+ActiveRecord::Schema.define(version: 20170904122209) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170903105051) do
     t.string   "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "reviews", force: :cascade do |t|
